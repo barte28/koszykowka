644b441ef6b3142d9cba00de6da452446c4430e8
barte28 <bartek.teuerle@gmail.com>
2025-03-20 08:17:01 +0100

    Binding

--- app/src/main/java/com/example/koszykowka/MainActivity.java
+++ app/src/main/java/com/example/koszykowka/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.koszykowka;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.koszykowka.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.textView.setText("87");
     }
+
+
+
 }
